fcbf4c3ccf64cf8cdaf7f221c63b1c4e13fe9894
徐俊杰 <yinhunfeixue@163.com>
2020-04-04 15:48:14 +0800

    docs:

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ConfirmButton from './components/ConfirmButton';
-import LimitUpload from './limitUpload/LimitUpload';
+// import LimitUpload from './limitUpload/LimitUpload';
 
 module.exports = {
   ConfirmButton,
-  LimitUpload,
+  // LimitUpload,
 };
