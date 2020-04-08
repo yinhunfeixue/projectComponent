f1fbed036dcbda5da0a35805a07328ef501eb00d
胡经山 <>
2020-04-08 17:11:13 +0800

    提交

--- src/IdCardLink.tsx
+++ src/IdCardLink.tsx
@@ -1,21 +1,44 @@
+import { Input } from 'antd';
 import React, { Component, ReactNode } from 'react';
-import { Col, Row } from 'antd';
+import lodash from 'lodash';
 import IComponentProps from './interfaces/IComponentProps';
+
+const IdCard = require('idcard');
 
 interface IIdCardLinkState {}
 interface IIdCardLinkProps extends IComponentProps {
-  form: any;
+  value?: string;
+  getIdCardInfo: (values: any) => void;
 }
 
 class IdCardLink extends Component<IIdCardLinkProps, IIdCardLinkState> {
+  componentDidMount() {
+    const { value } = this.props;
+    this.setIdCardInfo(value);
+  }
+
+  componentDidUpdate(prevProps: IIdCardLinkProps) {
+    if (!lodash.isEqual(this.props.value, prevProps.value)) {
+      const { value } = this.props;
+      this.setIdCardInfo(value);
+    }
+  }
+
+  setIdCardInfo = (value: any) => {
+    const { getIdCardInfo } = this.props;
+    if (value && IdCard.verify(value)) {
+      const info = IdCard.info(value);
+      getIdCardInfo(info);
+    }
+  };
+
+  onChange = (e: { target: { value: any } }) => {
+    const idCard = e.target.value;
+    this.setIdCardInfo(idCard);
+  };
+
   public render(): ReactNode {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Row>
-        <Col>
-        </Col>
-      </Row>
-    );
+    return <Input onChange={this.onChange} />;
   }
 }
 
