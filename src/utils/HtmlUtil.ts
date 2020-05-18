17e63637583dd21be1665dadd5c7fe8b58bd16c9
徐俊杰 <yinhunfeixue@163.com>
2020-05-18 16:09:37 +0800

    feat: 添加导出blob的方法

--- src/utils/HtmlUtil.ts
+++ src/utils/HtmlUtil.ts
@@ -7,6 +7,16 @@ class HtmlUtil {
     span.innerHTML = htmlStr;
     return span.innerText;
   }
+
+  static exportBlob(blob: Blob, fileName?: string) {
+    let url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    if (fileName) {
+      link.download = fileName;
+    }
+    link.click();
+  }
 }
 
 export default HtmlUtil;
