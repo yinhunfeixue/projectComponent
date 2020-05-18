277f97001ee44a6b2152848f1278109384829d8c
徐俊杰 <yinhunfeixue@163.com>
2020-05-18 16:18:20 +0800

    docs:

--- src/utils/HtmlUtil.ts
+++ src/utils/HtmlUtil.ts
@@ -1,4 +1,8 @@
 class HtmlUtil {
+  /**
+   * 富文本转换为纯文件
+   * @param htmlStr 富文本
+   */
   static plainHtml(htmlStr?: string) {
     if (!htmlStr) {
       return '';
@@ -8,6 +12,11 @@ class HtmlUtil {
     return span.innerText;
   }
 
+  /**
+   * 从blob中导出文件
+   * @param blob blob实例
+   * @param fileName 文件名
+   */
   static exportBlob(blob: Blob, fileName?: string) {
     let url = URL.createObjectURL(blob);
     const link = document.createElement('a');
