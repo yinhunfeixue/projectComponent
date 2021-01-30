6833b9b144da1f2af6b66dc8e169189adaea0d9b
徐俊杰 <yinhunfeixue@163.com>
2021-01-30 22:30:23 +0800

    feat: 修改项目配置

--- v0.1/.fatherrc.ts
+++ v0.1/.fatherrc.ts
@@ -2,4 +2,16 @@ export default {
   esm: 'babel',
   cjs: 'babel',
   lessInBabelMode: true,
+
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+      },
+      'antd',
+    ],
+  ],
 };
